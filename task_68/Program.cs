f90a0dd3cf703302544d9db62e83db891aca3525
Sergei <sandeeey@example.com>
2023-03-13 11:14:57 +0300

    task68

--- task_68/Program.cs
+++ task_68/Program.cs
@@ -12,8 +12,8 @@ int n = Convert.ToInt32(Console.ReadLine());
 int CalFunction(int num1, int num2)
 {
     if (num1 == 0) return (num2 + 1);
-    if (num2 == 0) return CalFunction( num1 - 1, 1);
-    return CalFunction( num1 - 1, CalFunction( num1 , num1-1));
-
+    else if (num2 == 0)  return CalFunction( num1 - 1, 1);
+     else return CalFunction( num1 - 1, CalFunction( num1 , num2-1));
+    
 }
 Console.WriteLine(CalFunction(m,n));
